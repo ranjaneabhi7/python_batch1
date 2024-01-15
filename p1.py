38727ea82706afe99a32735f4740d29a6f8e6f46
Abhijeet Ranjane <139632570+ranjaneabhi7@users.noreply.github.com>
2024-01-15 06:49:32 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,9 @@
+branch=input("Enter your branch: ")
+if branch=="CSE":
+    print("Exam is started from 23rd January")
+elif branch=="Mech":
+    print("Exam is started from 24th January")
+elif branch=="Civil":
+    print("Exam is started from 25th January")
+else:
+    print("Invalid Input")
